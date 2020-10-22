00255fbff727b457f71ee4c87659105231eef40d
Noa Lea <noa@noalea.work>
2020-10-22 12:59:09 -0400

    Add task to list

--- application.js
+++ application.js
@@ -1,29 +1,33 @@
-function getTasksUsingXHTTP() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      const container = document.getElementById('container');
-      const responses = JSON.parse(this.responseText);
-
-      for (let i = 0; i < responses.length; i++) {
-        const response = responses[i];
-        container.insertAdjacentHTML("beforeend", `<li>[${response.id}]: ${response.task}</li>`)
-      }
-    }
-  }
-  xhttp.open("GET", "http://localhost:3001/todos", true);
-  xhttp.send();
-}
-
-function getTasksUsingFetch() {
+function getTasks() {
   fetch('http://localhost:3001/todos')
   .then(response => response.json())
   .then(data => {
     for (let i = 0; i < data.length; i++) {
-      const response = data[i];
-      container.insertAdjacentHTML("beforeend", `<li>[${response.id}]: ${response.task}</li>`)
+      addTodoToList(data[i]);
     }
   });
 }
 
-getTasksUsingFetch();
+function sendForm(event) {
+  event.preventDefault();
+  const formData = new FormData(document.getElementById('todo_form'));
+  fetch('http://localhost:3001/todos', {
+    method: 'POST',
+    headers:  {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      task: formData.get('todo[task]')
+    })
+  })
+  .then(response => response.json())
+  .then(data => addTodoToList(data));
+}
+
+function addTodoToList(response) {
+  const container = document.getElementById('container');
+  container.insertAdjacentHTML("beforeend", `<li>[${response.id}]: ${response.task}</li>`);
+}
+
+getTasks();
